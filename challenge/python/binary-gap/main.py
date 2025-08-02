cb6c1179d6415e178223217c9d5811e05b3ab718
vitran96 <38664902+vitran96@users.noreply.github.com>
2025-08-02 20:25:58 +0700

    feat: binary gap solution

--- challenge/python/binary-gap/main.py
+++ challenge/python/binary-gap/main.py
@@ -1,14 +1,22 @@
-# BinaryGap
+class Solution(object):
+    def binaryGap(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        # This can be optimized with bitwise function
+        arr = list(bin(n))[2:]
 
-Find longest sequence of zeros in binary representation of an integer.
+        gap = 0
+        # Simplify the algo by checking last '1' only
+        last = -1
+        i = 0
+        while i < len(arr):
+            if arr[i] == '1':
+                if last >= 0:
+                    gap = max(gap, i - last)
+                last = i
 
----
+            i = i + 1
 
-A **binary gap** within a positive integer `N` is any maximal sequence of consecutive zeros that is surrounded by ones at both ends in the binary representation of `N`.
-
-For example:
-- Number `9` has binary representation `1001` and contains a binary gap of length **2**.
-- Number `529` has binary representation `1000010001` and contains two binary gaps: one of length **4** and one of length **3**.
-- Number `20` has binary representation `10100` and contains one binary gap of length **1**.
-- Number `15` has binary representation `1111` and has **no** binary gaps.
-- Number `32` has binary representation `100000` and has **no** binary gaps.
+        return gap
