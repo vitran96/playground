82befb46d9cb0eaff412f571a82c6f039532f4b0
vitran96 <38664902+vitran96@users.noreply.github.com>
2025-10-02 21:54:15 +0700

    refactor: change app name

--- study/spring-1/src/main/java/tech/kingoyster/spring_1/Spring1Application.java
+++ study/spring-1/src/main/java/tech/kingoyster/spring_1/Spring1Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Spring1Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(Spring1Application.class, args);
     }
 
 }
