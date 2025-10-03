3978d2bf91e1497bc1a22bf0d2b6cfc522d4bc4e
vitran96 <38664902+vitran96@users.noreply.github.com>
2025-10-03 14:47:12 +0700

    feat: implement base Customer CRUD

--- study/spring-1/src/main/java/tech/kingoyster/spring_1/controller/CustomersController.java
+++ study/spring-1/src/main/java/tech/kingoyster/spring_1/controller/CustomersController.java
@@ -2,35 +2,38 @@ package tech.kingoyster.spring_1.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.NotImplementedException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tech.kingoyster.spring_1.model.Customers;
 import tech.kingoyster.spring_1.service.ICustomersService;
 
 import java.util.List;
 
-@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/customers")
+@RequiredArgsConstructor
 public class CustomersController {
     private final ICustomersService customersService;
 
     @GetMapping
     public List<Customers> getAll() {
-        throw new NotImplementedException();
+        return customersService.getAll();
     }
 
     @GetMapping("/{id}")
-    public Customers getById(@PathVariable Long id) {
-        throw new NotImplementedException();
+    public ResponseEntity<Customers> getById(@PathVariable Long id) {
+        return customersService.getById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
     public Customers create(@RequestBody Customers customer) {
-        throw new NotImplementedException();
+        return customersService.create(customer);
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) {
-        throw new NotImplementedException();
+    public void deleteById(@PathVariable Long id) {
+        customersService.deleteById(id);
     }
 }
--- study/spring-1/src/main/java/tech/kingoyster/spring_1/service/ICustomersService.java
+++ study/spring-1/src/main/java/tech/kingoyster/spring_1/service/ICustomersService.java
@@ -1,4 +1,16 @@
 package tech.kingoyster.spring_1.service;
 
+import tech.kingoyster.spring_1.model.Customers;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface ICustomersService {
+    List<Customers> getAll();
+
+    Optional<Customers> getById(Long id);
+
+    Customers create(Customers customer);
+
+    void deleteById(Long id);
 }
