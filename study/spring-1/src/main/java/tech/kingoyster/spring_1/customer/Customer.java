d1bc97503a6c140b3e65d0f01cc57e7faa1d0e8e
vitran96 <38664902+vitran96@users.noreply.github.com>
2025-10-12 22:51:13 +0700

    feat: add email validation

--- study/spring-1/src/main/java/tech/kingoyster/spring_1/customer/Customer.java
+++ study/spring-1/src/main/java/tech/kingoyster/spring_1/customer/Customer.java
@@ -1,6 +1,7 @@
 package tech.kingoyster.spring_1.customer;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.Generated;
@@ -23,6 +24,7 @@ public class Customer implements java.io.Serializable {
     @Column(name = "name", nullable = false, length = 255)
     private String name;
 
+    @Email
     @Column(name = "email", nullable = false, unique = true, length = 255)
     private String email;
 
@@ -31,57 +33,4 @@ public class Customer implements java.io.Serializable {
     @Column(name="registered_at", insertable = false, updatable = false, length=19)
     @ColumnDefault("CURRENT_TIMESTAMP")
     private LocalDateTime registeredAt;
-
-//    public Customers(int id, String name, String email, Timestamp registeredAt) {
-//        this.id = id;
-//        this.name = name;
-//        this.email = email;
-//        this.registeredAt = registeredAt;
-//    }
-
-//     @Id
-//
-//
-//    @Column(name="id", unique=true, nullable=false)
-//    public int getId() {
-//        return this.id;
-//    }
-//
-//    public void setId(int id) {
-//        this.id = id;
-//    }
-//
-//
-//    @Column(name="name", nullable=false, length=255)
-//    public String getName() {
-//        return this.name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//
-//    @Column(name="email", nullable=false, length=255)
-//    public String getEmail() {
-//        return this.email;
-//    }
-//
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-//
-//    @Temporal(TemporalType.TIMESTAMP)
-//    @Column(name="registered_at", length=19)
-//    public Timestamp getRegisteredAt() {
-//        return this.registeredAt;
-//    }
-//
-//    public void setRegisteredAt(Timestamp registeredAt) {
-//        this.registeredAt = registeredAt;
-//    }
-
-
-
-
 }
