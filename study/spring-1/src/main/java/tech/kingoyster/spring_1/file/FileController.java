a7eab44f19e738964dc5f4842c964b376dd332f3
vitran96 <38664902+vitran96@users.noreply.github.com>
2026-01-17 11:00:50 +0700

    feat: add base upload/download api

---
+++ study/spring-1/src/main/java/tech/kingoyster/spring_1/file/FileController.java
@@ -0,0 +1,52 @@
+package tech.kingoyster.spring_1.file;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/files")
+@RequiredArgsConstructor
+public class FileController {
+
+    private final StorageService storageService;
+
+    @GetMapping
+    public List<String> listFile() {
+        return null;
+    }
+
+    @GetMapping(value = "/{filename:.+}", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public ResponseEntity<Resource> downloadFile(@PathVariable String filename) {
+        return null;
+    }
+
+//    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+//    @ResponseStatus(HttpStatus.NO_CONTENT)
+//    public void uploadFile(@RequestParam("file")MultipartFile file) throws IOException {
+//        var filename = file.getOriginalFilename();
+//        var data = file.getBytes();
+//
+//        // TODO:
+//    }
+
+    // NOTE: @RequestPart allow use to send both BLOB data & JSON
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void uploadFile(
+            @RequestPart("file") MultipartFile file,
+            @RequestPart("metadata") Map<String, String> additionalData) throws IOException {
+        var filename = file.getOriginalFilename();
+        var data = file.getBytes();
+
+        // TODO:
+    }
+}
---
+++ study/spring-1/src/main/java/tech/kingoyster/spring_1/file/StorageService.java
@@ -0,0 +1,9 @@
+package tech.kingoyster.spring_1.file;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class StorageService {
+}
