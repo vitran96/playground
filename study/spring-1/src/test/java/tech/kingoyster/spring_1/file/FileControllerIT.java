27b2df804a897b4afc06bdb8c9ad0e765bd1b2c8
vitran96 <38664902+vitran96@users.noreply.github.com>
2026-01-17 22:43:54 +0700

    feat: remove FileControllerTest and move it to FileControllerIT

--- study/spring-1/src/test/java/tech/kingoyster/spring_1/file/FileControllerIT.java
+++ study/spring-1/src/test/java/tech/kingoyster/spring_1/file/FileControllerIT.java
@@ -1,9 +1,22 @@
 package tech.kingoyster.spring_1.file;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import tech.kingoyster.spring_1.authentication.JwtProvider;
+
+import java.nio.file.Path;
 
 @SpringBootTest(properties = {
         "spring.liquibase.enabled=false", // Don't run migration
@@ -15,4 +28,38 @@ public class FileControllerIT {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockitoBean
+    private FileProperties fileProperties;
+
+    @Test
+    void whenUpload_thenNoError(@TempDir Path tempDir) throws Exception {
+        Mockito.when(fileProperties.getDirectory()).thenReturn(tempDir);
+        var mockFile = new MockMultipartFile(
+                "file",
+                "test.txt",
+                "text/plain",
+                "Test file".getBytes()
+        );
+
+        mockMvc.perform(MockMvcRequestBuilders.multipart("/api/v1/file").file(mockFile))
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
+
+    @Test
+    void whenDownload_thenReturnFile(@TempDir Path tempDir) throws Exception {
+        Mockito.when(fileProperties.getDirectory()).thenReturn(tempDir);
+        var filename = "file.txt";
+
+        var mockResult = mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/file/" + filename))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.header()
+                                   .string(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"file.txt\""))
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_OCTET_STREAM))
+                .andReturn();
+
+        byte[] contentAsByteArray = mockResult.getResponse().getContentAsByteArray();
+        Assertions.assertNotNull(contentAsByteArray);
+        Assertions.assertTrue(contentAsByteArray.length > 0);
+    }
 }
