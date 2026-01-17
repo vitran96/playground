cc138bcb6608398d4fcb7a722db798b7dc682fcd
vitran96 <38664902+vitran96@users.noreply.github.com>
2026-01-17 17:23:35 +0700

    backup: backup test code

--- study/spring-1/src/test/java/tech/kingoyster/spring_1/file/FileControllerIT.java
+++ study/spring-1/src/test/java/tech/kingoyster/spring_1/file/FileControllerIT.java
@@ -1,10 +1,18 @@
 package tech.kingoyster.spring_1.file;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-import tech.kingoyster.spring_1.MySqlTestConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Import(MySqlTestConfiguration.class)
+@SpringBootTest(properties = {
+        "spring.liquibase.enabled=false", // Don't run migration
+        "spring.jpa.hibernate.ddl-auto=none", // Don't validate JPA
+        "spring.jpa.database-platform=org.hibernate.dialect.MySQLDialect",
+})
+@AutoConfigureMockMvc(addFilters = false)
 public class FileControllerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
 }
--- study/spring-1/src/test/java/tech/kingoyster/spring_1/file/FileControllerTest.java
+++ study/spring-1/src/test/java/tech/kingoyster/spring_1/file/FileControllerTest.java
@@ -1,4 +1,24 @@
 package tech.kingoyster.spring_1.file;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(value = FileController.class,
+    excludeAutoConfiguration = SecurityAutoConfiguration.class)
 public class FileControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private StorageService storageService;
+
+//    @Test
+//    void whenUpload
 }
